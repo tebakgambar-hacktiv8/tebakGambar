7388615585edf145952159aa767860f2adadb720
justarya <just1arya@gmail.com>
2019-08-22 19:11:34 +0700

    Vuex Modular

--- client/src/modules/server.js
+++ client/src/modules/server.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import server from './modules/server'
-import view from './modules/view'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    view,server
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
   }
 })
